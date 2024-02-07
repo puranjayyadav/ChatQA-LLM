85bff3c250f30f7580b08327ec91896b563a0e95
Puranjay Yadav <puranjayrocks4@gmail.com>
2024-02-07 12:49:18 -0500

    Completed the project

---
+++ app/chat/chains/streamable.py
@@ -0,0 +1,18 @@
+from queue import Queue
+from threading import Thread
+from app.chat.callbacks.stream import StreamingHandler
+from flask import current_app
+class StreamableChain:
+    def stream(self,input):
+        queue = Queue()
+        handler = StreamingHandler(queue)
+        def task(app_context):
+            app_context.push()
+            self(input, callbacks=[handler])
+
+        Thread(target=task, args=[current_app.app_context()]).start()
+        while True:
+            token = queue.get()
+            if token is None:
+                break
+            yield token
